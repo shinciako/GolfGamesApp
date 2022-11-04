8a4d63d34493635aa61f6a1832e84c7d8393d2e5
shinciako <100320142+shinciako@users.noreply.github.com>
2022-11-04 19:50:04 +0100

    Receiving object GameType

--- app/src/main/java/com/example/golfgamesapp/ui/gamesType/games/Games.kt
+++ app/src/main/java/com/example/golfgamesapp/ui/gamesType/games/Games.kt
@@ -6,13 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.golfgamesapp.MainActivity
 import com.example.golfgamesapp.databinding.FragmentGamesBinding
+import com.example.golfgamesapp.ui.gamesType.GameType
 
 
 class Games : Fragment() {
     private var _binding: FragmentGamesBinding? = null
+    lateinit var input: GameType
+    private val navigationArgs : GamesArgs by navArgs()
     private var shortGamesList = listOf<Game>(
         Game("21 game",0,0),
         Game("Chipping zone challenge",1,0),
@@ -26,11 +30,10 @@ class Games : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentGamesBinding.inflate(inflater, container, false)
-        var input = arguments?.getInt("gameType")
-        var inputTitle = arguments?.getString("gameName")
-        (activity as MainActivity)?.setActionBarTitle(inputTitle)
 
-        shortGamesList = shortGamesList.filter {it.gameType==input}
+        input = navigationArgs.game
+        (activity as MainActivity)?.setActionBarTitle(input.name)
+        shortGamesList = shortGamesList.filter {it.gameType==input.type}
         val shortGames = binding.recyclerViewShortGames
         shortGames.layoutManager = LinearLayoutManager(activity)
         shortGames.adapter = ShortGamesRecyclerViewAdapter(
