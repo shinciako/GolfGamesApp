552f30cd9b1e40426458af4945c18714d22e0312
shinciako <100320142+shinciako@users.noreply.github.com>
2022-11-04 19:48:46 +0100

    Added parcelable

--- app/src/main/java/com/example/golfgamesapp/ui/gamesType/GameType.kt
+++ app/src/main/java/com/example/golfgamesapp/ui/gamesType/GameType.kt
@@ -1,4 +1,8 @@
 package com.example.golfgamesapp.ui.gamesType
 
-data class GameType(val name: String, val type: Int ) {
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class GameType(val name: String, val type: Int ) : Parcelable {
 }
