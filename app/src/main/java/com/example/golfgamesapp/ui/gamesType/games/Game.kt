029ebb1356a9572a44325665f7527fe88b82a097
shinciako <100320142+shinciako@users.noreply.github.com>
2022-11-06 23:01:17 +0100

    Parcelized data class Game

--- app/src/main/java/com/example/golfgamesapp/ui/gamesType/games/Game.kt
+++ app/src/main/java/com/example/golfgamesapp/ui/gamesType/games/Game.kt
@@ -1,3 +1,7 @@
 package com.example.golfgamesapp.ui.gamesType.games
 
-data class Game(val name: String, val id: Int, val gameType: Int)
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Game(val name: String, val id: Int, val gameType: Int) : Parcelable
