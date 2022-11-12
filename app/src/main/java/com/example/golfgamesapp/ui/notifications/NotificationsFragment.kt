907f27ccb2989f6c148d109347aa4fea14223778
shinciako <100320142+shinciako@users.noreply.github.com>
2022-11-12 17:11:47 +0100

    Added contact dev fragment

--- app/src/main/java/com/example/golfgamesapp/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/golfgamesapp/ui/notifications/NotificationsFragment.kt
@@ -1,20 +1,18 @@
 package com.example.golfgamesapp.ui.notifications
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.golfgamesapp.databinding.FragmentNotificationsBinding
+
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,17 +20,24 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
-
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        setupButtons()
+        return binding.root
+    }
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    private fun setupButtons(){
+        binding.ivFb.setOnClickListener{
+            redirect("https://www.facebook.com/suicideEnjoyer/")
         }
-        return root
+        binding.ivInsta.setOnClickListener{
+            redirect("https://www.instagram.com/shinciako/")
+        }
+    }
+
+    private fun redirect(url: String){
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
     }
 
     override fun onDestroyView() {
