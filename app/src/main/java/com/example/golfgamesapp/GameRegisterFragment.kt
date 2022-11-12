f4bb52575aaa5464b03598d88b284fa563877e86
shinciako <100320142+shinciako@users.noreply.github.com>
2022-11-12 17:15:36 +0100

    Added notifications

--- app/src/main/java/com/example/golfgamesapp/GameRegisterFragment.kt
+++ app/src/main/java/com/example/golfgamesapp/GameRegisterFragment.kt
@@ -1,22 +1,24 @@
 package com.example.golfgamesapp
 
-import android.app.Application
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.text.set
+import androidx.core.app.NotificationCompat
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.golfgamesapp.databinding.FragmentGameRegisterBinding
 import com.example.golfgamesapp.db.Game
 import com.example.golfgamesapp.db.GameDatabase
-import com.example.golfgamesapp.ui.gamesType.MyRecyclerViewAdapter
 import com.example.golfgamesapp.ui.gamesType.games.GameInfo
-import java.util.*
 
 
 class GameRegisterFragment : Fragment() {
@@ -29,33 +31,76 @@ class GameRegisterFragment : Fragment() {
     private lateinit var adapter: GameRecyclerViewAdapter
     private var isListItemClicked = false
 
+    private val channelID = "com.example.golfgamesapp.channel1"
+    private var notificationManager: NotificationManager? = null
+
 
     private lateinit var selectedGame: Game
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentGameRegisterBinding.inflate(inflater, container, false)
         input = navigationArgs.gameInfo
         (activity as MainActivity).setActionBarTitle(input.name)
 
+        setupDb()
+        setupNotifications()
+        setupButtons()
+        setupRv()
+        return binding.root
+    }
+
+
+    private fun setupDb(){
         val dao = GameDatabase.getInstance((activity as MainActivity).application).gameDao()
         val factory = GameViewModelFactory(dao)
         viewModel = ViewModelProvider(this, factory)[GameViewModel::class.java]
+    }
 
+    //Notifications related functions
+    private fun setupNotifications(){
+        notificationManager = requireActivity()
+            .getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel(channelID,"DemoChannel","this is a demo")
+    }
 
+    private fun createNotificationChannel(id : String, name:String, channelDescription:String){
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(id,name,importance).apply {
+            description = channelDescription
+        }
+        notificationManager?.createNotificationChannel(channel)
+    }
 
-        binding.btnSave.setOnClickListener(){
-            if(isListItemClicked){
-                updateGameData()
-                clearInput()
-            }else{
-                saveGameData()
-                clearInput()
+    private fun displayNotification(game: Game){
+        val notificationId = 45
+        val notification = NotificationCompat.Builder(requireActivity(),channelID)
+            .setContentTitle("Golf Games App")
+            .setContentText("You scored ${game.points} in ${game.name}")
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+        notificationManager?.notify(notificationId,notification)
+
+    }
+
+    //Button related functions
+    private fun setupButtons(){
+        binding.btnSave.setOnClickListener {
+            if(validatePoints()) {
+                if (isListItemClicked) {
+                    updateGameData()
+                    clearInput()
+                } else {
+                    saveGameData()
+                    clearInput()
+                }
             }
         }
-        binding.btnClear.setOnClickListener(){
+        binding.btnClear.setOnClickListener {
             if(isListItemClicked){
                 deleteGameData()
                 clearInput()
@@ -64,21 +109,26 @@ class GameRegisterFragment : Fragment() {
                 clearInput()
             }
         }
-        setupRv()
-        return binding.root
     }
-
 
     private fun saveGameData(){
         val points = binding.etPoints.text.toString().toInt()
         val date = binding.etDate.text.toString()
         val game = Game(0,input.name,points, date)
         viewModel.insertGame(game)
-        Toast.makeText(
-            this.context,
-            "You have added a new ${game.name} game",
-            Toast.LENGTH_SHORT
-        ).show()
+        displayNotification(game)
+    }
+
+    private fun validatePoints():Boolean{
+        if(binding.etPoints.text.isNullOrBlank()){
+            Toast.makeText(
+                this.context,
+                "Input must be integer",
+                Toast.LENGTH_SHORT
+            ).show()
+            return false
+        }
+        return true
     }
 
     private fun clearInput(){
@@ -124,6 +174,7 @@ class GameRegisterFragment : Fragment() {
         displayGameList()
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     private fun displayGameList(){
         viewModel.games.observe(viewLifecycleOwner) {
             adapter.setList(it)
@@ -139,5 +190,4 @@ class GameRegisterFragment : Fragment() {
         binding.etPoints.setText(selectedGame.points.toString())
         binding.etDate.setText(selectedGame.date)
     }
-
 }
